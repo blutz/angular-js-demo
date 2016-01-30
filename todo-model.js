826af3e9613d96549cec1419c6908de28c8bb575
Byron Lutz <byronlutz@gmail.com>
2016-01-30 05:52:36 -0800

    Added localStorage

--- todo-model.js
+++ todo-model.js
@@ -1,24 +1,46 @@
 (function(){
   function todoModel () {
     var self = this;
-    this._items = [
-      'Eat pizza',
-      'Get dinner',
-      'Buy a dodge',
-    ];
+    this._items = getFromLocalStorage();
+
     this.getItems = function() {
       return self._items;
     };
     this.addItem = function(item) {
       if(!item) { return; };
-      self._items.push(item);
+      self.getItems().push(item);
+      saveToLocalStorage();
     };
     this.removeItem = function(item) {
-      var index = self._items.indexOf(item);
+      var index = self.getItems().indexOf(item);
       if(index >= 0) {
-        self._items.splice(index, 1);
+        self.getItems().splice(index, 1);
       }
+      saveToLocalStorage();
     };
+
+    function getFromLocalStorage() {
+      if (!isLocalStorageAvailable()) { return []; }
+      return JSON.parse(window.localStorage.todoItems);
+    }
+
+    function saveToLocalStorage() {
+      if(!isLocalStorageAvailable()) { return false; }
+      window.localStorage.todoItems = JSON.stringify(self._items);
+      return true;
+    }
+
+    function isLocalStorageAvailable() {
+      try {
+        var storage = window['localStorage'];
+        var x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+      } catch(e) {
+        return false;
+      }
+    }
   };
   angular.module('todoApp')
   .service('todoModel', todoModel);
