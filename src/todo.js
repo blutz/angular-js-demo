9cd3c8cbd59727edd63019c3aa566a2f412eab36
Byron Lutz <byronlutz@gmail.com>
2016-01-30 08:44:32 -0800

    Added routing

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,20 @@
 (function() {
-  angular.module('todoApp', [ ]);
+  var todoApp = angular.module('todoApp', ['ngRoute']);
+
+  todoApp.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/', {
+          templateUrl: '/partials/todo.html',
+          controller: 'todoController'
+        }).
+        when('/stats', {
+          templateUrl: '/partials/todo-stats.html',
+          controller: 'todoController'
+        }).
+        otherwise({
+          redirectTo: '/'
+        });
+    }
+  ]);
 })();
